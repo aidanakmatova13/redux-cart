349764076be4ac4547f249b3daa41c0a8165494e
aidanakmatova13 <akmatova.don@gmail.com>
2021-10-01 15:21:25 +0300

    add error message

--- src/Views/Cart/index.js
+++ src/Views/Cart/index.js
@@ -1,57 +1,66 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Layout from "../../Components/Layout";
 import {useSelector, useDispatch} from "react-redux";
+import './style.css'
 
 const Cart = () => {
     const dispatch = useDispatch()
     const cart = useSelector(store => store.cart)
-    const [counter, setCounter] = useState(0)
-    const increase = () => {
-
-    }
 
     return (
         <Layout>
             <div className='container'>
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">Название</th>
-                        <th scope="col">Цена</th>
-                        <th scope="col">Количество</th>
-                        <th scope="col">Удалить</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {cart.map(el =>
-                        <tr>
-                            <td>{el.title}</td>
-                            <td>{el.price}$</td>
-                            <td>
-                                <button className='btn-danger'
-                                        onClick={() => dispatch({type: 'DECREASE_TO_ONE', payload: el})}>-
-                                </button>
-                                {el.quantity}
-                                <button className='btn-primary'
-                                        onClick={() => dispatch({type: 'ADD_TO_CART', payload: el})}>+
-                                </button>
-                            </td>
-                            <td>
-                                <button className='btn btn-danger'
-                                        onClick={() => dispatch({type: 'REMOVE_FROM_CART', payload: el.id})}>Удалить
-                                </button>
-                            </td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
-                <h5>Итого:
-                    {
-                        cart.reduce((acc, el) => {
-                            return (el.price * el.quantity) + acc
-                        }, 0).toFixed(2)
-                    }
-                </h5>
+                {
+                    cart.length ? <>
+                            <table className="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Название</th>
+                                    <th scope="col">Описание</th>
+                                    <th scope="col">Цена</th>
+                                    <th scope="col">Количество</th>
+                                    <th scope="col">Удалить</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {cart.map(el =>
+                                    <tr>
+                                        <td>{el.title}</td>
+                                        <td>{el.description}</td>
+                                        <td>{el.price}$</td>
+                                        <td>
+                                            <button className='btn-danger'
+                                                    onClick={() => dispatch({type: 'DECREASE_TO_ONE', payload: el})}>-
+                                            </button>
+                                            {el.quantity}
+                                            <button className='btn-primary'
+                                                    onClick={() => dispatch({type: 'ADD_TO_CART', payload: el})}>+
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button className='btn btn-danger'
+                                                    onClick={() => dispatch({
+                                                        type: 'REMOVE_FROM_CART',
+                                                        payload: el.id
+                                                    })}>Удалить
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )}
+                                </tbody>
+                            </table>
+                            <h5>Итого:
+                                {
+                                    cart.reduce((acc, el) => {
+                                        return (el.price * el.quantity) + acc
+                                    }, 0).toFixed(2)
+                                }
+                            </h5>
+                        </>
+                        : <h3 className='error-cart mt-4'>
+                            Корзина пуста!
+                        </h3>
+                }
             </div>
         </Layout>
     );
